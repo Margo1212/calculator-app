f9feac5eda5dd68cbce154f070c4269b0de6f3b9
Margo1212 <sirachenko944@gmail.com>
2021-08-18 18:47:42 +0200

    Adding functionality for buttons of number

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const numbers = document.querySelectorAll('.num')
+const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.btn-result')
+const reset = document.querySelector('.btn-reset')
+const clean = document.querySelector('.btn-del')
+const numPrev = document.querySelector('.previous_number')
+const numCurr = document.querySelector('.current_number')
+
+let previousOperation = ''
+let currentOperation = ''
+let operation = undefined
+
+const updateResult = () => {
+    numCurr.innerText = currentOperation
+    numPrev.innerText = previousOperation
+}
+
+const addNum = (num) => {
+    if(currentOperation.includes('.')) {
+        return
+    }
+    currentOperation = currentOperation.toString() + num.toString()
+}
+
+const deleteNumber = () => {
+    currentOperation = currentOperation.toString().slice(0, -1)
+}
+
+numbers.forEach((num) => {
+    num.addEventListener('click', () => {
+        addNum(num.innerText)
+        updateResult()
+    })
+})
+
+clean.addEventListener('click', () => {
+    deleteNumber()
+    updateResult()
+})
+
+
